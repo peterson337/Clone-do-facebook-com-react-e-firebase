87de9ffe473eacbf713e23aee9b73869a31af5b5
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2023-04-19 18:04:35 -0300

    Projeto em desenvolvimento

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -29,3 +29,29 @@ function App() {
 }
 
 export default App
+
+/* 
+19:48
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC5kdX3TUStnT4qYXtFtF2ijWK1RkTFPVg",
+  authDomain: "clone-facebook-8aeba.firebaseapp.com",
+  projectId: "clone-facebook-8aeba",
+  storageBucket: "clone-facebook-8aeba.appspot.com",
+  messagingSenderId: "380030812338",
+  appId: "1:380030812338:web:2f776d7c3b0ae801cd89ab",
+  measurementId: "G-MZ392XMFM4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+*/
