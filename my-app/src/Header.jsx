7e97335616c29fd3f92a2a13711e45c773586c79
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2023-04-22 14:26:51 -0300

    Projeto em desenvolvimento

--- my-app/src/Header.jsx
+++ my-app/src/Header.jsx
@@ -1,26 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {FaFacebook, FaSearch, FaAlignJustify} from "react-icons/fa";
 
 const Header = () => {
-  return (
+    
+  return (    
     <div className='header'>
     <div className='headerLeft'>
+      
         <div className='logo-fb'>
-          <FaFacebook/>
+        <a href="#">
+        <FaFacebook></FaFacebook></a>
         </div>
+
         <div className='search-fb'>
         <FaSearch/>
         </div>
+
         <div className='menu-fb'>
         <FaAlignJustify/>
         </div>
     </div>
+    
     <div className='headerRight'>
       <div className='plusBtn'>
         +
       </div>
-    </div>
-  </div>  )
+      </div>
+
+      <div className='backgroundModal'>
+        <div className='modal'>
+
+        </div>
+      </div>
+
+  </div>  
+  )
 }
 
 export default Header
